1363696eb3be35d55c7622350274f273e351b495
Gustavo <andrescabral.c@gmail.com>
2017-08-03 01:06:50 -0400

    chore: New dependencies and configuration tools

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const eslint = require('gulp-eslint')
+const nodemon = require('gulp-nodemon')
+
+const paths = {
+  srcApp: 'src/index.js',
+  srcFiles: 'src/**/*.js'
+}
+
+gulp.task('lint', () =>
+  gulp.src(paths.srcFiles).pipe(eslint()).pipe(eslint.format())
+)
+
+gulp.task('serve', ['lint'], () =>
+  nodemon({
+    exec: 'node --inspect',
+    script: paths.srcApp,
+    tasks: 'lint',
+    watch: paths.srcFiles
+  })
+)
+
+gulp.task('default', ['serve'])
