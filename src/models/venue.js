9e8c3b0194f31dc2deade9b4c7294a5f5f95733a
Gustavo <andrescabral.c@gmail.com>
2017-11-03 16:36:34 -0300

    refactor(venues): edit review related fields to add more granularity

--- src/models/venue.js
+++ src/models/venue.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose')
 
 const venueSchema = new mongoose.Schema(
   {
+    allowsGuideDog: {
+      yes: {
+        type: Number,
+        default: 0
+      },
+      no: {
+        type: Number,
+        default: 0
+      }
+    },
     bathroomReviews: {
       type: Number,
       default: 0,
@@ -9,9 +19,8 @@ const venueSchema = new mongoose.Schema(
     },
     bathroomScore: {
       type: Number,
-      default: 0,
       max: [5, 'Should be less than 6'],
-      min: [0, 'Should be more than -1']
+      min: [1, 'Should be more than 0']
     },
     entryReviews: {
       type: Number,
@@ -20,40 +29,84 @@ const venueSchema = new mongoose.Schema(
     },
     entryScore: {
       type: Number,
-      default: 0,
       max: [5, 'Should be less than 6'],
-      min: [0, 'Should be more than -1']
+      min: [1, 'Should be more than 0']
     },
-    guideDog: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
+    hasParking: {
+      yes: {
+        type: Number,
+        default: 0
+      },
+      no: {
+        type: Number,
+        default: 0
+      }
+    },
+    hasRamp: {
+      yes: {
+        type: Number,
+        default: 0
+      },
+      no: {
+        type: Number,
+        default: 0
+      }
+    },
+    hasSecondEntry: {
+      yes: {
+        type: Number,
+        default: 0
+      },
+      no: {
+        type: Number,
+        default: 0
+      }
+    },
+    hasWellLit: {
+      yes: {
+        type: Number,
+        default: 0
+      },
+      no: {
+        type: Number,
+        default: 0
+      }
     },
     isArchived: {
       type: Boolean,
       default: false,
       required: [true, 'Is required']
     },
-    location: {
-      latitude: {
+    isQuiet: {
+      yes: {
         type: Number,
-        max: [90, 'Should be less than 91'],
-        min: [-90, 'Should be more than -91']
+        default: 0
       },
-      longitude: {
+      no: {
         type: Number,
-        max: [180, 'Should be less than 181'],
-        min: [-180, 'Should be more than -181']
+        default: 0
       }
+    },
+    isSpacious: {
+      yes: {
+        type: Number,
+        default: 0
+      },
+      no: {
+        type: Number,
+        default: 0
+      }
+    },
+    location: {
+      type: {
+        type: String,
+        default: 'Point'
+      },
+      coordinates: [Number]
     },
     name: {
       type: String,
       maxlength: [255, 'Should be less than 256 characters']
-    },
-    parking: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
     },
     photos: [
       {
@@ -118,35 +171,29 @@ const venueSchema = new mongoose.Schema(
       maxlength: [255, 'Should be less than 256 characters'],
       required: [true, 'Is required']
     },
-    quiet: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
-    },
-    ramp: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
-    },
     reviews: [
       {
         type: mongoose.Schema.ObjectId,
         ref: 'Review'
       }
     ],
-    secondEntry: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
-    },
-    spacious: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
-    },
-    stepsReviews: {
-      type: [Number],
-      default: [0, 0, 0, 0]
+    steps: {
+      zero: {
+        type: Number,
+        default: 0
+      },
+      one: {
+        type: Number,
+        default: 0
+      },
+      two: {
+        type: Number,
+        default: 0
+      },
+      moreThanTwo: {
+        type: Number,
+        default: 0
+      }
     },
     types: [
       {
@@ -157,16 +204,16 @@ const venueSchema = new mongoose.Schema(
     vicinity: {
       type: String,
       maxlength: [255, 'Should be less than 256 characters']
-    },
-    wellLit: {
-      type: Number,
-      default: 0,
-      min: [0, 'Should be more than -1']
     }
   },
   { timestamps: true }
 )
 
-venueSchema.index({ placeId: 1 })
+venueSchema.index({
+  name: 'text',
+  location: '2dsphere',
+  placeId: 1,
+  vicinity: 'text'
+})
 
 module.exports = mongoose.model('Venue', venueSchema)
--- src/routes/venues/list-venues.js
+++ src/routes/venues/list-venues.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
-const { find } = require('lodash')
+const { find, isEmpty } = require('lodash')
 
+const { isNumber } = require('../../helpers')
 const logger = require('../../helpers/logger')
 const Venue = require('../../models/venue')
 
@@ -14,99 +15,272 @@ module.exports = async (req, res, next) => {
     return res.status(400).json(errors)
   }
 
-  let placesQueryString = `?key=${process.env.PLACES_API_KEY}`
-  if (!queryParams.page) {
-    placesQueryString = `${placesQueryString}&location=${queryParams.location}&rankby=distance`
+  let venuesQuery = {}
 
-    if (queryParams.keyword) {
-      placesQueryString = `${placesQueryString}&keyword=${queryParams.keyword}`
+  if (queryParams.bathroomScore) {
+    venuesQuery.bathroomScore = {
+      $gte: queryParams.bathroomScore,
+      $lt: queryParams.bathroomScore + 1
+    }
+  }
+
+  if (queryParams.entryScore) {
+    venuesQuery.entryScore = {
+      $gte: queryParams.entryScore,
+      $lt: queryParams.entryScore + 1
+    }
+  }
+
+  if (typeof queryParams.allowsGuideDog !== 'undefined') {
+    const allowsGuideDog = parseFloat(queryParams.allowsGuideDog) === 1
+    if (allowsGuideDog) {
+      venuesQuery.allowsGuideDog.yes = { $gte: 1 }
+    } else {
+      venuesQuery.allowsGuideDog.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.hasParking !== 'undefined') {
+    const hasParking = parseFloat(queryParams.hasParking) === 1
+    if (hasParking) {
+      venuesQuery.hasParking.yes = { $gte: 1 }
+    } else {
+      venuesQuery.hasParking.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.hasRamp !== 'undefined') {
+    const hasRamp = parseFloat(queryParams.hasRamp) === 1
+    if (hasRamp) {
+      venuesQuery.hasRamp.yes = { $gte: 1 }
+    } else {
+      venuesQuery.hasRamp.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.hasSecondEntry !== 'undefined') {
+    const hasSecondEntry = parseFloat(queryParams.hasSecondEntry) === 1
+    if (hasSecondEntry) {
+      venuesQuery.hasSecondEntry.yes = { $gte: 1 }
+    } else {
+      venuesQuery.hasSecondEntry.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.hasWellLit !== 'undefined') {
+    const hasWellLit = parseFloat(queryParams.hasWellLit) === 1
+    if (hasWellLit) {
+      venuesQuery.hasWellLit.yes = { $gte: 1 }
+    } else {
+      venuesQuery.hasWellLit.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.isQuiet !== 'undefined') {
+    const isQuiet = parseFloat(queryParams.isQuiet) === 1
+    if (isQuiet) {
+      venuesQuery.isQuiet.yes = { $gte: 1 }
+    } else {
+      venuesQuery.isQuiet.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.isSpacious !== 'undefined') {
+    const isSpacious = parseFloat(queryParams.isSpacious) === 1
+    if (isSpacious) {
+      venuesQuery.isSpacious.yes = { $gte: 1 }
+    } else {
+      venuesQuery.isSpacious.no = { $gte: 1 }
+    }
+  }
+
+  if (typeof queryParams.steps !== 'undefined') {
+    if (parseFloat(queryParams.steps) === 0) {
+      venuesQuery.steps.zero = { $gte: 1 }
+    } else if (parseFloat(queryParams.steps) === 1) {
+      venuesQuery.steps.one = { $gte: 1 }
+    } else if (parseFloat(queryParams.steps) === 2) {
+      venuesQuery.steps.two = { $gte: 1 }
+    } else if (parseFloat(queryParams.steps) === 3) {
+      venuesQuery.steps.moreThanTwo = { $gte: 1 }
+    }
+  }
+
+  let dataResponse
+
+  if (!isEmpty(venuesQuery)) {
+    if (queryParams.keywords) {
+      venuesQuery.$text = { $search: queryParams.keywords }
     }
 
-    if (queryParams.language) {
-      placesQueryString = `${placesQueryString}&language=${queryParams.language}`
-    } else {
-      placesQueryString = `${placesQueryString}&language=en`
+    const coordinates = queryParams.location.split(',')
+    queryParams.location = {
+      $near: {
+        $geometry: {
+          type: 'Point',
+          coordinates: [coordinates[1], coordinates[0]]
+        },
+        $maxDistance: 5000
+      }
     }
 
     if (queryParams.type) {
-      placesQueryString = `${placesQueryString}&type=${queryParams.type}`
+      venuesQuery.types = queryParams.type
+    }
+
+    venuesQuery.isArchived = false
+
+    let page = 1
+    if (isNumber(queryParams.page)) {
+      page = queryParams.page
+    }
+
+    const pageLimit = 6
+
+    if (page > 0) {
+      page -= 1
     } else {
-      placesQueryString = `${placesQueryString}&type=establishment`
+      return res
+        .status(400)
+        .json({ page: 'Should be equal to or greater than 1' })
+    }
+
+    let total
+    let venues
+    try {
+      ;[venues, total] = await Promise.all([
+        Venue.find(venuesQuery)
+          .select(
+            '-__v -bathroomReviews -createdAt -entryReviews -isArchived -reviews -updatedAt'
+          )
+          .skip(page * pageLimit)
+          .limit(pageLimit),
+        Venue.find(venuesQuery).count()
+      ])
+    } catch (err) {
+      logger.error(
+        `Venues failed to be found or count at list-venues.\nvenuesQuery: ${JSON.stringify(
+          venuesQuery
+        )}`
+      )
+      return next(err)
+    }
+
+    const lastPage = Math.ceil(total / pageLimit)
+    let last = `${process.env.API_URL}/venues?page=${lastPage}`
+    if (lastPage > 0) {
+      page += 1
+      if (page > lastPage) {
+        return res
+          .status(400)
+          .json({ page: `Should be equal to or less than ${lastPage}` })
+      }
+    } else {
+      last = null
+      page = null
+    }
+
+    dataResponse = {
+      last,
+      page,
+      results: venues
     }
   } else {
-    placesQueryString = `${placesQueryString}&pagetoken=${queryParams.page}`
-  }
+    let placesQuery = `?key=${process.env.PLACES_API_KEY}`
 
-  let placesResponse
-  try {
-    placesResponse = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json${placesQueryString}`
-    )
-  } catch (err) {
-    logger.error(
-      `Places failed to be found at list-venues.\nQuery Params: ${JSON.stringify(
-        queryParams
-      )}`
-    )
-    return next(err)
-  }
+    if (!queryParams.page) {
+      placesQuery = `${placesQuery}&location=${queryParams.location}&rankby=distance`
 
-  let places = []
-  const placesIds = []
-  placesResponse.data.results.forEach(place => {
-    let photo = ''
-    if (place.photos) {
-      photo = `https://maps.googleapis.com/maps/api/place/photo?key=${process
-        .env.PLACES_API_KEY}&maxwidth=300&photoreference=${place.photos[0]
-        .photo_reference}`
-    }
-
-    places.push({
-      generalScore: place.rating,
-      icon: place.icon,
-      location: {
-        latitude: place.geometry.location.lat,
-        longitude: place.geometry.location.lng
-      },
-      name: place.name,
-      photo,
-      placeId: place.place_id,
-      types: place.types
-    })
-    placesIds.push(place.place_id)
-  })
-
-  let venues
-  try {
-    venues = await Venue.find({ placeId: { $in: placesIds } })
-  } catch (err) {
-    logger.error(
-      `Venues failed to be found at list-venues.\nPlaces ids: [${placesIds}]`
-    )
-    return next(err)
-  }
-
-  places = places.map(place => {
-    const venue = find(venues, venue => venue.placeId === place.placeId)
-    if (venue) {
-      let photo = place.photo
-      if (venue.photos) {
-        photo = venues.photos[0].url
+      if (queryParams.keywords) {
+        placesQuery = `${placesQuery}&keyword=${queryParams.keywords}`
       }
 
-      return Object.assign({}, place, {
-        bathroomScore: venue.bathroomScore,
-        entryScore: venue.entryScore,
-        photo
-      })
+      if (queryParams.language) {
+        placesQuery = `${placesQuery}&language=${queryParams.language}`
+      } else {
+        placesQuery = `${placesQuery}&language=en`
+      }
+
+      if (queryParams.type) {
+        placesQuery = `${placesQuery}&type=${queryParams.type}`
+      } else {
+        placesQuery = `${placesQuery}&type=establishment`
+      }
+    } else {
+      placesQuery = `${placesQuery}&pagetoken=${queryParams.page}`
     }
 
-    return place
-  })
+    let placesResponse
+    try {
+      placesResponse = await axios.get(
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json${placesQuery}`
+      )
+    } catch (err) {
+      logger.error(
+        `Places failed to be found at list-venues.\nQuery Params: ${JSON.stringify(
+          queryParams
+        )}`
+      )
+      return next(err)
+    }
 
-  const dataResponse = {
-    nextPage: placesResponse.data.next_page_token,
-    results: places
+    let places = []
+    const placesIds = []
+    placesResponse.data.results.forEach(place => {
+      let photo = ''
+      if (place.photos) {
+        photo = `https://maps.googleapis.com/maps/api/place/photo?key=${process
+          .env.PLACES_API_KEY}&maxwidth=300&photoreference=${place.photos[0]
+          .photo_reference}`
+      }
+
+      places.push({
+        generalScore: place.rating,
+        icon: place.icon,
+        location: {
+          latitude: place.geometry.location.lat,
+          longitude: place.geometry.location.lng
+        },
+        name: place.name,
+        photo,
+        placeId: place.place_id,
+        types: place.types
+      })
+      placesIds.push(place.place_id)
+    })
+
+    let venues
+    try {
+      venues = await Venue.find({ placeId: { $in: placesIds } })
+    } catch (err) {
+      logger.error(
+        `Venues failed to be found at list-venues.\nPlaces ids: [${placesIds}]`
+      )
+      return next(err)
+    }
+
+    places = places.map(place => {
+      const venue = find(venues, venue => venue.placeId === place.placeId)
+      if (venue) {
+        let photo = place.photo
+        if (venue.photos) {
+          photo = venues.photos[0].url
+        }
+
+        return Object.assign({}, place, {
+          bathroomScore: venue.bathroomScore,
+          entryScore: venue.entryScore,
+          photo
+        })
+      }
+
+      return place
+    })
+
+    dataResponse = {
+      nextPage: placesResponse.data.next_page_token,
+      results: places
+    }
   }
 
   return res.status(200).json(dataResponse)
