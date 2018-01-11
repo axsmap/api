2850327372513841109159f96a15333efb440645
Gustavo <andrescabral.c@gmail.com>
2018-01-11 00:40:28 -0300

    feat(petitions): add list petitions endpoint

--- src/routes/petitions/index.js
+++ src/routes/petitions/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const { isAuthenticated, isUnblocked } = require('../../helpers')
 
 const createPetition = require('./create-petition')
+const listPetitions = require('./list-petitions')
 
 const router = new express.Router()
 
+router.get('', isAuthenticated, isUnblocked, listPetitions)
 router.post('', isAuthenticated, isUnblocked, createPetition)
 
 module.exports = router
--- src/routes/petitions/list-petitions.js
+++ src/routes/petitions/list-petitions.js
@@ -1,68 +1,17 @@
-const { Event } = require('../../models/event')
 const logger = require('../../helpers/logger')
 const { Petition } = require('../../models/petition')
-const { Team } = require('../../models/team')
 
 module.exports = async (req, res, next) => {
-  if (req.user.isBlocked) {
-    return res.status(423).json({ general: 'You are blocked' })
-  }
-
   const queryParams = req.query
 
-  let managedEventsIds = []
-  if (req.user.events && req.user.events.length > 0) {
-    const eventsPromises = req.user.events.map(e =>
-      Event.findOne({ _id: e.toString() })
-    )
-
-    let userEvents
-    try {
-      userEvents = await Promise.all(eventsPromises)
-    } catch (err) {
-      logger.error('An event failed to be found at list-petitions')
-      return next(err)
-    }
-
-    const managedEvents = userEvents.filter(e =>
-      e.managers.find(m => m.toString() === req.user.id)
-    )
-    managedEventsIds = managedEvents.map(e => e.id)
-  }
-
-  let managedTeamsIds = []
-  if (req.user.teams && req.user.teams.length > 0) {
-    const teamsPromises = req.user.teams.map(t =>
-      Team.findOne({ _id: t.toString() })
-    )
-
-    let userTeams
-    try {
-      userTeams = await Promise.all(teamsPromises)
-    } catch (err) {
-      logger.error('A team failed to be found at list-petitions')
-      return next(err)
-    }
-
-    const managedTeams = userTeams.filter(t =>
-      t.managers.find(m => m.toString() === req.user.id)
-    )
-    managedTeamsIds = managedTeams.map(t => t.id)
-  }
-
   const petitionsQuery = {
-    $or: [
-      { receiverId: req.user.id },
-      { receiverId: { $in: managedEventsIds } },
-      { receiverId: { $in: managedTeamsIds } },
-      { senderId: req.user.id },
-      { senderId: { $in: managedTeamsIds } }
-    ]
+    $or: [{ sender: req.user.id }, { user: req.user.id }]
   }
+
+  const sortBy = '-createdAt'
 
   let page = queryParams.page || 1
-  const pageLimit = 18
-
+  const pageLimit = 12
   if (page > 0) {
     page -= 1
   } else {
@@ -76,8 +25,8 @@ module.exports = async (req, res, next) => {
   try {
     ;[petitions, total] = await Promise.all([
       Petition.find(petitionsQuery)
-        .select('-__v -updatedAt -createdAt')
-        .sort('createdAt')
+        .select('-__v -updatedAt')
+        .sort(sortBy)
         .skip(page * pageLimit)
         .limit(pageLimit),
       Petition.find(petitionsQuery).count()
@@ -87,9 +36,7 @@ module.exports = async (req, res, next) => {
     return next(err)
   }
 
-  let first = `${process.env.API_URL}/petitions?page=1`
-  const lastPage = Math.ceil(total / pageLimit)
-  let last = `${process.env.API_URL}/petitions?page=${lastPage}`
+  let lastPage = Math.ceil(total / pageLimit)
   if (lastPage > 0) {
     page += 1
     if (page > lastPage) {
@@ -98,17 +45,16 @@ module.exports = async (req, res, next) => {
         .json({ page: `Should be equal to or less than ${lastPage}` })
     }
   } else {
-    first = null
-    last = null
     page = null
+    lastPage = null
   }
 
   return res.status(200).json({
-    first,
-    last,
     page,
+    lastPage,
     pageLimit,
-    results: petitions,
-    total
+    total,
+    sortBy,
+    results: petitions
   })
 }
