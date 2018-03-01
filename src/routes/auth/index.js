a779396258f62b9bf979e55b3aa3a2fd13f0c51a
Gustavo <andrescabral.c@gmail.com>
2018-03-01 15:46:06 -0300

    refactor(auth): remove brute force middlewares

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const ExpressBrute = require('express-brute')
-const MongooseStore = require('express-brute-mongoose')
 
 const { isAuthenticated } = require('../../helpers')
-const BruteForce = require('../../models/brute-force')
 
 const activateAccount = require('./activate-account')
 const facebookSignIn = require('./facebook-sign-in')
@@ -16,21 +13,15 @@ const signOut = require('./sign-out')
 const signUp = require('./sign-up')
 
 const router = new express.Router()
-const store = new MongooseStore(BruteForce)
-const bruteforce = new ExpressBrute(store, {
-  freeRetries: 2,
-  minWait: 500,
-  maxWait: 15 * 60 * 1000
-})
 
 router.get('/activate-account/:key', activateAccount)
-router.post('/facebook', bruteforce.prevent, facebookSignIn)
-router.post('/forgotten-password', bruteforce.prevent, forgottenPassword)
-router.post('/google', bruteforce.prevent, googleSignIn)
-router.put('/reset-password', bruteforce.prevent, resetPassword)
-router.post('/sign-in', bruteforce.prevent, signIn)
+router.post('/facebook', facebookSignIn)
+router.post('/forgotten-password', forgottenPassword)
+router.post('/google', googleSignIn)
+router.put('/reset-password', resetPassword)
+router.post('/sign-in', signIn)
 router.delete('/sign-out', isAuthenticated({ isOptional: false }), signOut)
-router.post('/sign-up', bruteforce.prevent, signUp)
-router.post('/token', bruteforce.prevent, generateToken)
+router.post('/sign-up', signUp)
+router.post('/token', generateToken)
 
 module.exports = router
