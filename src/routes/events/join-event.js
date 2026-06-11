ddacee6e4d44eb899ea696d9af1dc8b399c74190
Jason Dasilva <jasondasilva@Jasons-MacBook-Pro.local>
2026-06-11 11:28:49 -0400

    Fix direct mapathon joining and location lookup

--- src/routes/events/join-event.js
+++ src/routes/events/join-event.js
@@ -1,7 +1,6 @@
 const moment = require('moment');
 
 const { Event } = require('../../models/event');
-const { Petition } = require('../../models/petition');
 
 module.exports = async (req, res, next) => {
   const eventId = req.params.eventId;
@@ -36,99 +35,30 @@ module.exports = async (req, res, next) => {
       .json({ general: 'You already are a participant in this event' });
   }
 
-  if (event.isOpen) {
-    req.user.events = [...req.user.events, event.id];
-    req.user.updatedAt = moment.utc().toDate();
-
-    try {
-      await req.user.save();
-    } catch (err) {
-      console.log(`User ${req.user.id} failed to be updated at join-event`);
-      return next(err);
-    }
-
-    event.participants = [...event.participants, req.user.id];
-    event.updatedAt = moment.utc().toDate();
-
-    try {
-      await event.save();
-    } catch (err) {
-      console.log(`Event ${event.id} failed to be updated at join-event`);
-      return next(err);
-    }
-
-    return res.status(200).json({ general: 'Joined' });
-  } else {
-    let petition;
-    try {
-      petition = await Petition.findOne({
-        event: event.id,
-        sender: req.user.id,
-        type: 'request-user-event'
-      });
-    } catch (err) {
-      console.log(
-        `Petition from user ${req.user.id} to event ${
-          event.id
-        } failed to be found at join-event`
-      );
-      return next(err);
-    }
-
-    if (petition && petition.state === 'pending') {
-      return res.status(400).json({
-        general: 'You already have a pending petition with this event'
-      });
-    }
-
-    if (
-      petition &&
-      (petition.state === 'rejected' || petition.state === 'canceled')
-    ) {
-      try {
-        await petition.remove();
-      } catch (err) {
-        console.log(
-          `Petition ${petition.id} failed to be removed at join-event`
-        );
-        return next(err);
-      }
-    }
-
-    const endDate = moment(event.endDate).utc();
-    const today = moment.utc();
-    if (endDate.isBefore(today)) {
-      return res
-        .status(423)
-        .json({ general: 'This event has already finished' });
-    }
-
-    const petitionData = {
-      event: event.id,
-      sender: req.user.id,
-      type: 'request-user-event'
-    };
-    try {
-      await Petition.create(petitionData);
-    } catch (err) {
-      if (typeof err.errors === 'object') {
-        const validationErrors = {};
-
-        Object.keys(err.errors).forEach(key => {
-          validationErrors[key] = err.errors[key].message;
-        });
-
-        return res.status(400).json(validationErrors);
-      }
-
-      console.log(
-        `Petition failed to be created at join-event.\nData: ${JSON.stringify(
-          petitionData
-        )}`
-      );
-      return next(err);
-    }
-
-    return res.status(200).json({ general: 'Requested' });
+  const endDate = moment(event.endDate).utc();
+  if (endDate.isBefore(moment.utc())) {
+    return res.status(423).json({ general: 'This event has already finished' });
   }
+
+  req.user.events = [...req.user.events, event.id];
+  req.user.updatedAt = moment.utc().toDate();
+
+  try {
+    await req.user.save();
+  } catch (err) {
+    console.log(`User ${req.user.id} failed to be updated at join-event`);
+    return next(err);
+  }
+
+  event.participants = [...event.participants, req.user.id];
+  event.updatedAt = moment.utc().toDate();
+
+  try {
+    await event.save();
+  } catch (err) {
+    console.log(`Event ${event.id} failed to be updated at join-event`);
+    return next(err);
+  }
+
+  return res.status(200).json({ general: 'Joined' });
 };
