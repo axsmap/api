b15c7fee8bc6d4d524a256d071b09f8e6565e015
Jason Dasilva <jasondasilva@Jasons-MBP.lan>
2026-06-20 12:05:16 -0400

    Add staging mapathon location search routes

--- src/routes/venues/index.js
+++ src/routes/venues/index.js
@@ -3,13 +3,17 @@ const express = require("express");
 const { isAuthenticated } = require("../../helpers");
 
 const archiveVenue = require("./archive-venue");
+const getPlaceLocation = require("./get-place-location");
 const getVenue = require("./get-venue");
 const listVenues = require("./list-venues");
+const searchPlaces = require("./search-places");
 const venueDetails = require("./venue-details");
 
 const router = new express.Router();
 
 router.get("", isAuthenticated({ isOptional: true }), listVenues);
+router.get("/places/autocomplete", searchPlaces);
+router.get("/places/:placeId", getPlaceLocation);
 // Place the more specific route before the generic "/:placeId" so it doesn't get
 // captured by the parameterized route. If "/:placeId" is first, requests like
 // "/detail/XYZ" are treated as placeId="detail" and never reach the detail
